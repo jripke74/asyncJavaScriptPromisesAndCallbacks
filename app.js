458ebdab4d39656793ff483b214200254e6b7996
jripke74 <jripke74@gmail.com>
2020-06-06 07:27:33 -0700

    add console.log

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const button = document.querySelector('button');
 const output = document.querySelector('p');
 
 function trackUserHandler() {
-
+  console.log('Clicked!');
 }
 
 button.addEventListener('click', trackUserHandler);
