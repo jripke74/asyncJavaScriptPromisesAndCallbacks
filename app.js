cd56010422bbf2c6f2cab72b11669f09165c7c7b
jripke74 <jripke74@gmail.com>
2020-06-06 07:16:09 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const button = document.querySelector('button');
+const output = document.querySelector('p');
+
+function trackUserHandler() {
+
+}
+
+button.addEventListener('click', trackUserHandler);
